501474a2d633e9bddf6c669094ed22dd5d1eac83
MongHeeee <rioisboy@naver.com>
2024-06-09 18:57:04 +0900

    make navi

---
+++ Assets/Controller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;  // 정확한 네임스페이스 사용
+
+public class Controller : MonoBehaviour
+{
+    public NavMeshAgent agent;  // NavMeshAgent 컴포넌트 참조
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 게임 시작 시 (0, 0, 0) 위치로 이동 설정
+        agent.SetDestination(new Vector3(0, 0, 0));
+    }
+}
